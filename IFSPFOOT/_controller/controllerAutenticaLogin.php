f571ec9758332b266b001caa90bbc0d7ebd9dfb2
Wesley Almeida da Silva <wsyalmeida@gmail.com>
2016-03-04 18:58:23 -0300

    Consulta na tabela Login para obter id, depois registra na variavel de sessão

--- IFSPFOOT/_controller/controllerAutenticaLogin.php
+++ IFSPFOOT/_controller/controllerAutenticaLogin.php
@@ -3,6 +3,9 @@
 	//Inclusão do arquivo para conexão com o banco de dados PDO
 	include_once '../_model/_bancodedados/modelBancodeDados.php';
 	
+	//Iniciando sessãoo
+	session_start();
+	
 	//Variaveis do index.php
 	$usuario = $_GET['textUsuario'];
 	$senha = $_GET['passwordSenha'];
@@ -15,12 +18,21 @@
 	$stm->execute();
 	$dados = $stm->fetch(PDO::FETCH_OBJ);
 	
+
 	//Se o resultado da consulta for diferente de vazio, então registra sessão e chama o cabecalho
 	if(!empty($dados)){
+		
+		//Registra na sessão id do usuario
+		$consultaId = 'SELECT id FROM Login WHERE usuario = ?';
+		$preparaConsultaId = $conn->prepare($consultaId);
+		$preparaConsultaId->bindValue(1, $usuario);
+		$preparaConsultaId->execute();
+		$idDono = $preparaConsultaId->fetchColumn(0);
 	
-		session_start();
-		$_SESSION['usuario']=$usuario;
-		$_SESSION['senha']=$senha;
+		$_SESSION['usuario']= $usuario;
+		$_SESSION['senha']= $senha;
+		$_SESSION['idDono']= $idDono;
+		
 		header("Location: ../_view/viewInicial.php");
 	 
 	}

fc55636e61b9085ddfa07407bd2019058dd5d0cf
Wesley Almeida da Silva <wsyalmeida@gmail.com>
2016-03-04 20:31:16 -0300

    Chamamento de arquivo que irá zerar o banco de dado ao clicar em novo

--- IFSPFOOT/_model/modelCadastrarTime.php
+++ IFSPFOOT/_model/modelCadastrarTime.php
@@ -3,6 +3,9 @@
 	//Inclusão do arquivo para conexão com o banco de dados PDO
 	include_once '../_model/_bancodedados/modelBancodeDados.php';
 	
+	//Zerar tabelas novo jogo
+	include_once '../_model/modelZerarTabela.php';
+	
 	//Variaveis da viewNovoJogo.php
 	$nomeTime    = $_GET['textNomeTime'];
 	$mascote  = $_GET['textNomeMascote'];
@@ -11,23 +14,10 @@
 	
 	//Inicio de Sessão para pegar usuario logado, usuário é único
 	session_start();
-	$usuario = $_SESSION['usuario'];
-	
-	//Consulta na Tabela Login para pegar id do usuário logado,assim inserimos ele como dono
-	$consultaDono = 'SELECT id FROM Login WHERE usuario = ?';
-	$prepraraDono = $conn->prepare($consultaDono);
-	$prepraraDono->bindValue(1, $usuario);
-	$prepraraDono->execute();
-	$dono = $prepraraDono->fetchColumn(0);
-	
-	//Consulta Tabela time para pegar último id do time cadastrado, campo de autoincremento
-	$consultaUltimoIdTime = 'SELECT id FROM Time ORDER BY id DESC';
-	$preparaIdTime = $conn->query($consultaUltimoIdTime);
-	$ultimoIdTime = $preparaIdTime->fetchColumn(0);
-	
-	//Pegando último time cadastrado para incrementar 1
-    $idInseriTime = $ultimoIdTime + 1;
-    
+	//$usuario = $_SESSION['usuario'];
+
+	$dono = $_SESSION['idDono'];
+		
     //Regra do Jogo
     $dinheiroInicial = "10,00";
     $torcidaInicial = "10";
@@ -51,11 +41,8 @@
   	<?php
 
   		//Inserção do um novo time
-  		$insercaoNovoTime = "INSERT INTO Time VALUES ($idInseriTime,'$nomeTime','$mascote','$cor','$dono','$dinheiroInicial','$torcidaInicial','$nomeEstadio','$capacidade')";
+  		$insercaoNovoTime = "INSERT INTO Time VALUES (1,'$nomeTime','$mascote','$cor','$dono','$dinheiroInicial','$torcidaInicial','$nomeEstadio','$capacidade')";
   		$conn->exec($insercaoNovoTime);
-
-  		session_start();
-		$_SESSION['idDono']= $dono;
   		
 		header("LOCATION: ../_controller/controllerGerenciaInicio.php");
 	

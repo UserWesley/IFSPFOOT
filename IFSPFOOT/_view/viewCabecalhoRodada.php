49f645a6a7689be93b4825618b7ac11ff713ef00
Wesley Almeida da Silva <wsyalmeida@gmail.com>
2016-03-05 12:16:02 -0300

    Seleciona Rodada para buscar no banco

---
+++ IFSPFOOT/_view/viewCabecalhoRodada.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<title>Página Inicial</title>
+
+</head>
+
+<body>
+	
+ 
+	<form action= "viewRodada.php">
+	
+		<label for= "idRodada">Selecione Rodada :</label>
+		<select name="rodada" id="idRodada">
+
+			<option value="1"selected>Rodada 1</option>
+    		<option value="2">Rodada 2</option>
+    		<option value="3">Rodada 3</option>   
+  		
+  		</select>
+  
+  		<input type="submit" value= "Mostrar">
+ 	
+ 	</form>
+
+ 	</body>
+
+ </html>

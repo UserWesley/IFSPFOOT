799b02f18ac32aac877bd722800a0e89df627cd6
Wesley Almeida da Silva <wsyalmeida@gmail.com>
2016-03-05 08:22:08 -0300

    Cadastro Automatico do campeonato

---
+++ IFSPFOOT/_model/modelCadastrarCampeonatoAutomatico.php
@@ -0,0 +1,10 @@
+<?php
+	
+	//Inclusão do arquivo para conexão com o banco de dados PDO
+	include_once '../_model/_bancodedados/modelBancodeDados.php';
+	
+	//Cadastro do campeonato inicial
+	$insercaoNovoCampeonato = "INSERT INTO Campeonato VALUES (1,'LIGA IFSP', '2016','')";
+	$conn->exec($insercaoNovoCampeonato);
+	
+?>

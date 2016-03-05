1a471a672d2328ccf6c8fbeff2f7fc5354bf64c9
Wesley Almeida da Silva <wsyalmeida@gmail.com>
2016-03-05 07:41:43 -0300

    Adicionado novos campos : posicao, nacionalidade e habilidade

--- IFSPFOOT/_model/modelCadastrarJogadoresAutomatico.php
+++ IFSPFOOT/_model/modelCadastrarJogadoresAutomatico.php
@@ -3,48 +3,48 @@
 	//Inclusão do arquivo para conexão com o banco de dados PDO
 	include_once '../_model/_bancodedados/modelBancodeDados.php';
 	
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (1,'JogadorTime1','JogadorTime1','10','10','1','10','Equilibrado','0')";
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (1,'JogadorTime1','JogadorTime1','Atacante','brasileiro','habilidade','10','10','1','10','Equilibrado','0')";
 	$conn->exec($insercaoNovoJogador);
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (2,'JogadorTime1','JogadorTime1','10','10','1','10','Equilibrado','0')";
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (2,'JogadorTime1','JogadorTime1','Atacante','brasileiro','habilidade','10','10','1','10','Equilibrado','0')";
 	$conn->exec($insercaoNovoJogador);
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (3,'JogadorTime1','JogadorTime1','30','50','1','100','Equilibrado','0')";
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (3,'JogadorTime1','JogadorTime1','Atacante','brasileiro','habilidade','30','50','1','100','Equilibrado','0')";
 	$conn->exec($insercaoNovoJogador);
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (4,'JogadorTime1','JogadorTime1','30','50','1','100','Equilibrado','0')";
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (4,'JogadorTime1','JogadorTime1','Atacante','brasileiro','habilidade','30','50','1','100','Equilibrado','0')";
 	$conn->exec($insercaoNovoJogador);
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (5,'JogadorTime1','JogadorTime1','30','50','1','100','Equilibrado','0')";
-	$conn->exec($insercaoNovoJogador);
-	
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (6,'JogadorTime2','JogadorTime2','30','50','2','100','Equilibrado','0')";
-	$conn->exec($insercaoNovoJogador);
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (7,'JogadorTime2','JogadorTime2','30','50','2','100','Equilibrado','0')";
-	$conn->exec($insercaoNovoJogador);
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (8,'JogadorTime2','JogadorTime2','30','50','2','100','Equilibrado','0')";
-	$conn->exec($insercaoNovoJogador);
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (9,'JogadorTime2','JogadorTime2','30','50','2','100','Equilibrado','0')";
-	$conn->exec($insercaoNovoJogador);
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (10,'JogadorTime2','JogadorTime2','30','50','2','100','Equilibrado','0')";
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (5,'JogadorTime1','JogadorTime1','Atacante','brasileiro','habilidade','30','50','1','100','Equilibrado','0')";
 	$conn->exec($insercaoNovoJogador);
 	
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (11,'JogadorTime3','JogadorTime3','30','50','3','100','Equilibrado','0')";
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (6,'JogadorTime2','JogadorTime2','Atacante','brasileiro','habilidade','30','50','2','100','Equilibrado','0')";
 	$conn->exec($insercaoNovoJogador);
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (12,'JogadorTime3','JogadorTime3','30','50','3','100','Equilibrado','0')";
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (7,'JogadorTime2','JogadorTime2','Atacante','brasileiro','habilidade','30','50','2','100','Equilibrado','0')";
 	$conn->exec($insercaoNovoJogador);
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (13,'JogadorTime3','JogadorTime3','30','50','3','100','Equilibrado','0')";
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (8,'JogadorTime2','JogadorTime2','Atacante','brasileiro','habilidade','30','50','2','100','Equilibrado','0')";
 	$conn->exec($insercaoNovoJogador);
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (14,'JogadorTime3','JogadorTime3','30','50','3','100','Equilibrado','0')";
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (9,'JogadorTime2','JogadorTime2','Atacante','brasileiro','habilidade','30','50','2','100','Equilibrado','0')";
 	$conn->exec($insercaoNovoJogador);
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (15,'JogadorTime3','JogadorTime3','30','50','3','100','Equilibrado','0')";
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (10,'JogadorTime2','JogadorTime2','Atacante','brasileiro','habilidade','30','50','2','100','Equilibrado','0')";
 	$conn->exec($insercaoNovoJogador);
 	
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (16,'JogadorTime4','JogadorTime4','30','50','4','100','Equilibrado','0')";
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (11,'JogadorTime3','JogadorTime3','Atacante','brasileiro','habilidade','30','50','3','100','Equilibrado','0')";
 	$conn->exec($insercaoNovoJogador);
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (17,'JogadorTime4','JogadorTime4','30','50','4','100','Equilibrado','0')";
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (12,'JogadorTime3','JogadorTime3','Atacante','brasileiro','habilidade','30','50','3','100','Equilibrado','0')";
 	$conn->exec($insercaoNovoJogador);
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (18,'JogadorTime4','JogadorTime4','30','50','4','100','Equilibrado','0')";
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (13,'JogadorTime3','JogadorTime3','Atacante','brasileiro','habilidade','30','50','3','100','Equilibrado','0')";
 	$conn->exec($insercaoNovoJogador);
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (19,'JogadorTime4','JogadorTime4','30','50','4','100','Equilibrado','0')";
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (14,'JogadorTime3','JogadorTime3','Atacante','brasileiro','habilidade','30','50','3','100','Equilibrado','0')";
 	$conn->exec($insercaoNovoJogador);
-	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (20,'JogadorTime4','JogadorTime4','30','50','4','100','Equilibrado','0')";
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (15,'JogadorTime3','JogadorTime3','Atacante','brasileiro','habilidade','30','50','3','100','Equilibrado','0')";
+	$conn->exec($insercaoNovoJogador);
+	
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (16,'JogadorTime4','JogadorTime4','Atacante','brasileiro','habilidade','30','50','4','100','Equilibrado','0')";
+	$conn->exec($insercaoNovoJogador);
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (17,'JogadorTime4','JogadorTime4','Atacante','brasileiro','habilidade','30','50','4','100','Equilibrado','0')";
+	$conn->exec($insercaoNovoJogador);
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (18,'JogadorTime4','JogadorTime4','Atacante','brasileiro','habilidade','30','50','4','100','Equilibrado','0')";
+	$conn->exec($insercaoNovoJogador);
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (19,'JogadorTime4','JogadorTime4','Atacante','brasileiro','habilidade','30','50','4','100','Equilibrado','0')";
+	$conn->exec($insercaoNovoJogador);
+	$insercaoNovoJogador = "INSERT INTO Jogador VALUES (20,'JogadorTime4','JogadorTime4','Atacante','brasileiro','habilidade','30','50','4','100','Equilibrado','0')";
 	$conn->exec($insercaoNovoJogador);
 	
 ?>

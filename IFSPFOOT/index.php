51ba8bb7e298c1e8da02a6f3f2db7698b80f6e3b
Wesley Almeida da Silva <wsyalmeida@gmail.com>
2016-03-14 20:00:15 -0300

    Adicionado Arquivo Jquery

--- IFSPFOOT/index.php
+++ IFSPFOOT/index.php
@@ -6,9 +6,22 @@
 <html lang="PT-BR">
 
 <head>
+
 	<meta charset= "UTF-8"/>
 	<title>Página Inicial</title>
-
+	
+	<!-- Visualização Mobile" -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- Incluindo Bootstrap CSS -->
+	<link href="_bootstrap-3.3.6-dist/_css/bootstrap.min.css" rel="stylesheet" media="screen">
+	
+	<!-- Incluindo Bootstrap JavaScript-->
+	<script src="_bootstrap-3.3.6-dist/_js/bootstrap.min.js"></script>
+	
+	<!-- Incluindo jquery-->
+	<script src="_jquery/jquery.js"></script>
+	
 </head>
 
 <body>

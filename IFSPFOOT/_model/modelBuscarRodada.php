fea1b306e31f3213a06d0a5249a0190031f33e00
Wesley Almeida da Silva <wsyalmeida@gmail.com>
2016-03-17 18:24:44 -0300

    mudancas devido novo layout

---
+++ IFSPFOOT/_model/modelBuscarRodada.php
@@ -0,0 +1,29 @@
+<?php
+
+//Inclusão do arquivo para conexão com o banco de dados PDO
+include_once '../_model/_bancodedados/modelBancodeDados.php';
+
+$consultaJogosRodada = 'SELECT Jogo.timeCasa, Jogo.golCasa, Jogo.golVisitante, Jogo.timeVisitante,
+Rodada.data, Rodada.hora, Rodada.periodo FROM Jogo,Rodada WHERE Jogo.rodada = Rodada.numero and Rodada.numero = ?';
+$preparaConsultaJogosRodada = $conn->prepare($consultaJogosRodada);
+$preparaConsultaJogosRodada->bindValue(1,$rodada);
+$preparaConsultaJogosRodada->execute();
+
+session_start();
+
+$result = $preparaConsultaJogosRodada->setFetchMode(PDO::FETCH_NUM);
+while ($row = $preparaConsultaJogosRodada->fetch()) {
+		
+	echo '<tr class = "active">';
+	echo "<td>{$_SESSION['timeCasa1'] = $row[0];}</td>";
+	echo "<td>{$row[1]}</td>";
+	echo "<td> X </td>";
+	echo "<td>{$row[2]}</td>";
+	echo "<td>{$row[3]}</td>";
+	echo "<td>{$row[4]}</td>";
+	echo "<td>{$row[5]}</td>";
+	echo "<td>{$row[6]}</td>";
+	echo '</tr>';
+}
+
+?>
--- IFSPFOOT/_model/modelCadastrarTimesAutomatico.php
+++ IFSPFOOT/_model/modelCadastrarTimesAutomatico.php
@@ -4,6 +4,10 @@
 	include_once '../_model/_bancodedados/modelBancodeDados.php';
 	
 	//Cadastro dos Times Automatico
+	
+	$insercaoNovoTime = "INSERT INTO Time VALUES (1,'Time1','mascote1','cor1',null,'10,00','10','time1','10',0,0,0,0)";
+	$conn->exec($insercaoNovoTime);
+	
 	$insercaoNovoTime = "INSERT INTO Time VALUES (2,'Time2','mascote2','cor2',null,'10,00','10','time2','10',0,0,0,0)";
   	$conn->exec($insercaoNovoTime);
   	$insercaoNovoTime = "INSERT INTO Time VALUES (3,'Time3','mascote3','cor3',null,'10,00','10','time3','10',0,0,0,0)";

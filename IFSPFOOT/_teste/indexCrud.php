57ef68627b0f045473a8a505651b07dde13be2d4
รąмueℓ g.ℓ <samuel_smgl@hotmail.com>
2016-03-19 23:02:19 -0300

    Atualizando CRUD's com JS
    Atribuindo imagens a CRUD's utilizando Java Script

--- IFSPFOOT/_teste/indexCrud.php
+++ IFSPFOOT/_teste/indexCrud.php
@@ -1,29 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>CRUD</title>
+	<title>IFSPFOOT</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="stylesheet" href="css/style.css" />
-	 <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
-	  <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-      <script type="text/javascript">	  
-        $(document).ready(function(){
-			$("#calcular").click(function(){
-				$.post("_view/viewCrudListarCampeonato.php", {"": $("#").val()},
-					function(data){
-						$("#resultado").html(data);
-					}
-				);
-			});
-		});
-	  </script>	  
+	<link rel="stylesheet" href="css/style.css" /> 
 </head>
 <body>
+<h3>CRUD's</h3>
+<h1>IFSPFOOT</h1>
 <center>
-<h1>CRUD</h1>
 <table>
-
     <thead>
         <tr>
           <th><h4>CADASTROS</h4></th>
@@ -32,23 +19,23 @@
 	</thead>
     <tbody>		
 	<tr>
-	<td><center><a  href="_view/viewCrudCadastrarCampeonato.php">CADASTRAR CAPEONATO</center></a></td>
-	<td><center><a  href="_view/viewCrudListarCampeonato.php">CONSULTAR CAMPEONATOS</center></a></td>
+	<td><a href="_view/viewCrudCadastrarCampeonato.php">CADASTRAR CAPEONATO</a></td>
+	<td><a href="_view/viewCrudListarCampeonato.php">CONSULTAR CAMPEONATOS</a></td>
 	</tr>
-	<td><center><a href="_view/viewCrudCadastrarRodada.php">CADASTRAR RODADA</center></a></td>
-	<td><center><a href="_view/viewCrudListarRodada.php">CONSULTAR RODADAS</center></a></td>
+	<td><a href="_view/viewCrudCadastrarRodada.php">CADASTRAR RODADA</a></td>
+	<td><a href="_view/viewCrudListarRodada.php">CONSULTAR RODADAS</a></td>
 	</tr>
-	<td><center><a href="_view/viewCrudCadastrarTime.php">CADASTRAR TIME</center></a></td>
-	<td><center><a href="_view/viewCrudListarTime.php">CONSULTAR TIMES</center></a></td>
+	<td><a href="_view/viewCrudCadastrarTime.php">CADASTRAR TIME</a></td>
+	<td><a href="_view/viewCrudListarTime.php">CONSULTAR TIMES</a></td>
 	</tr>		
-	<td><center><a href="_view/viewCrudCadastrarJogador.php">CADASTRAR JOGADOR</center></a></td>
-	<td><center><a href="_view/viewCrudListarJogador.php">CONSULTAR JOGADORES</center></a></td>	
+	<td><a href="_view/viewCrudCadastrarJogador.php">CADASTRAR JOGADOR</a></td>
+	<td><a href="_view/viewCrudListarJogador.php">CONSULTAR JOGADORES</a></td>	
 	</tr>	
-	<td><center><a href="_view/viewCrudCadastrarJogo.php">CADASTRAR JOGO</center></a></td>
-	<td><center><a href="_view/viewCrudListarJogo.php">CONSULTAR JOGOS</center></a></td>	
+	<td><a href="_view/viewCrudCadastrarJogo.php">CADASTRAR JOGO</a></td>
+	<td><a href="_view/viewCrudListarJogo.php">CONSULTAR JOGOS</a></td>
 	</tbody>
 </table>
-<div id="resultado"></div>
+<script type="text/javascript" src="js/slideshow.js"></script>
 </center>
 </body>
 </html>

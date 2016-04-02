27063e66ec042dce9ae69ecb953f0776ea3baef9
Wesley Almeida da Silva <wsyalmeida@gmail.com>
2016-04-02 20:46:38 -0300

    Prepara cada rodada

---
+++ IFSPFOOT/_controller/controllerPreparaJogo.php
@@ -0,0 +1,9 @@
+<?php
+ 
+	//Inclusão do arquivo para recolher dados do jogos da rodada
+	include_once '../_model/modelRecolherDadosJogosRodada.php';
+
+	//Inclusão do arquivo para recolher dados do jogos da rodada
+	//include_once '../_model/modelRecolherDadosTimeRodada.php';
+	header("Location: ../_view/viewJogo.php");
+?>

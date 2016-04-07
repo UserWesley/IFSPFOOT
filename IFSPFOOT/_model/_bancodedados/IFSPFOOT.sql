65a6dda18e31e61228d833db793862d4ecbc8583
Wesley Almeida da Silva <wsyalmeida@gmail.com>
2016-04-07 20:33:08 -0300

    Alteração do banco de dados add colunas

--- IFSPFOOT/_model/_bancodedados/IFSPFOOT.sql
+++ IFSPFOOT/_model/_bancodedados/IFSPFOOT.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Tempo de geração: 05/03/2016 às 16:16
+-- Tempo de geração: 23/03/2016 às 21:59
 -- Versão do servidor: 10.1.9-MariaDB
 -- Versão do PHP: 5.6.15
 
@@ -33,13 +33,6 @@ CREATE TABLE `Campeonato` (
   `vencedor` varchar(20) COLLATE utf32_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_bin;
 
---
--- Fazendo dump de dados para tabela `Campeonato`
---
-
-INSERT INTO `Campeonato` (`id`, `nome`, `temporada`, `vencedor`) VALUES
-(1, 'LIGA IFSP', 2016, NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -61,32 +54,6 @@ CREATE TABLE `Jogador` (
   `gol` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_bin;
 
---
--- Fazendo dump de dados para tabela `Jogador`
---
-
-INSERT INTO `Jogador` (`id`, `nome`, `sobrenome`, `posicao`, `nacionalidade`, `habilidade`, `idade`, `forca`, `idTime`, `estamina`, `nivel`, `gol`) VALUES
-(1, 'JogadorTime1', 'JogadorTime1', 'Atacante', 'brasileiro', 'habilidade', 10, 10, 1, 10, 'Equilibrado', 0),
-(2, 'JogadorTime1', 'JogadorTime1', 'Atacante', 'brasileiro', 'habilidade', 10, 10, 1, 10, 'Equilibrado', 0),
-(3, 'JogadorTime1', 'JogadorTime1', 'Atacante', 'brasileiro', 'habilidade', 30, 50, 1, 100, 'Equilibrado', 0),
-(4, 'JogadorTime1', 'JogadorTime1', 'Atacante', 'brasileiro', 'habilidade', 30, 50, 1, 100, 'Equilibrado', 0),
-(5, 'JogadorTime1', 'JogadorTime1', 'Atacante', 'brasileiro', 'habilidade', 30, 50, 1, 100, 'Equilibrado', 0),
-(6, 'JogadorTime2', 'JogadorTime2', 'Atacante', 'brasileiro', 'habilidade', 30, 50, 2, 100, 'Equilibrado', 0),
-(7, 'JogadorTime2', 'JogadorTime2', 'Atacante', 'brasileiro', 'habilidade', 30, 50, 2, 100, 'Equilibrado', 0),
-(8, 'JogadorTime2', 'JogadorTime2', 'Atacante', 'brasileiro', 'habilidade', 30, 50, 2, 100, 'Equilibrado', 0),
-(9, 'JogadorTime2', 'JogadorTime2', 'Atacante', 'brasileiro', 'habilidade', 30, 50, 2, 100, 'Equilibrado', 0),
-(10, 'JogadorTime2', 'JogadorTime2', 'Atacante', 'brasileiro', 'habilidade', 30, 50, 2, 100, 'Equilibrado', 0),
-(11, 'JogadorTime3', 'JogadorTime3', 'Atacante', 'brasileiro', 'habilidade', 30, 50, 3, 100, 'Equilibrado', 0),
-(12, 'JogadorTime3', 'JogadorTime3', 'Atacante', 'brasileiro', 'habilidade', 30, 50, 3, 100, 'Equilibrado', 0),
-(13, 'JogadorTime3', 'JogadorTime3', 'Atacante', 'brasileiro', 'habilidade', 30, 50, 3, 100, 'Equilibrado', 0),
-(14, 'JogadorTime3', 'JogadorTime3', 'Atacante', 'brasileiro', 'habilidade', 30, 50, 3, 100, 'Equilibrado', 0),
-(15, 'JogadorTime3', 'JogadorTime3', 'Atacante', 'brasileiro', 'habilidade', 30, 50, 3, 100, 'Equilibrado', 0),
-(16, 'JogadorTime4', 'JogadorTime4', 'Atacante', 'brasileiro', 'habilidade', 30, 50, 4, 100, 'Equilibrado', 0),
-(17, 'JogadorTime4', 'JogadorTime4', 'Atacante', 'brasileiro', 'habilidade', 30, 50, 4, 100, 'Equilibrado', 0),
-(18, 'JogadorTime4', 'JogadorTime4', 'Atacante', 'brasileiro', 'habilidade', 30, 50, 4, 100, 'Equilibrado', 0),
-(19, 'JogadorTime4', 'JogadorTime4', 'Atacante', 'brasileiro', 'habilidade', 30, 50, 4, 100, 'Equilibrado', 0),
-(20, 'JogadorTime4', 'JogadorTime4', 'Atacante', 'brasileiro', 'habilidade', 30, 50, 4, 100, 'Equilibrado', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -101,18 +68,6 @@ CREATE TABLE `Jogo` (
   `timeVisitante` varchar(20) COLLATE utf32_bin NOT NULL,
   `rodada` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_bin;
-
---
--- Fazendo dump de dados para tabela `Jogo`
---
-
-INSERT INTO `Jogo` (`id`, `timeCasa`, `golCasa`, `golVisitante`, `timeVisitante`, `rodada`) VALUES
-(1, 'dsa', NULL, NULL, 'Time2', 1),
-(2, 'dsa', NULL, NULL, 'Time3', 2),
-(3, 'dsa', NULL, NULL, 'Time4', 3),
-(4, 'Time2', NULL, NULL, 'Time3', 3),
-(5, 'Time2', NULL, NULL, 'Time4', 2),
-(6, 'Time3', NULL, NULL, 'Time4', 1);
 
 -- --------------------------------------------------------
 
@@ -130,14 +85,6 @@ CREATE TABLE `Login` (
   `email` varchar(40) COLLATE utf32_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_bin COMMENT='Login de Autenticação a aplicação';
 
---
--- Fazendo dump de dados para tabela `Login`
---
-
-INSERT INTO `Login` (`id`, `usuario`, `senha`, `administrador`, `nome`, `sobrenome`, `email`) VALUES
-(1, 'a', 'a', 1, 'a', 'a', 'a'),
-(2, 'b', 'b', 0, 'a', 'a', 'b');
-
 -- --------------------------------------------------------
 
 --
@@ -153,15 +100,6 @@ CREATE TABLE `Rodada` (
   `clima` varchar(20) COLLATE utf32_bin NOT NULL,
   `campeonato` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_bin;
-
---
--- Fazendo dump de dados para tabela `Rodada`
---
-
-INSERT INTO `Rodada` (`id`, `numero`, `data`, `hora`, `periodo`, `clima`, `campeonato`) VALUES
-(1, 1, '2016-03-10', '16:00', 'Tarde', 'Nublado', 1),
-(2, 2, '2016-04-10', '20:00', 'Noite', 'Chuva', 1),
-(3, 3, '2016-05-10', '09:00', 'ManhÃ£', 'Sol', 1);
 
 -- --------------------------------------------------------
 
@@ -184,16 +122,6 @@ CREATE TABLE `Time` (
   `empate` int(11) NOT NULL,
   `pontos` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_bin COMMENT='Time';
-
---
--- Fazendo dump de dados para tabela `Time`
---
-
-INSERT INTO `Time` (`id`, `nome`, `mascote`, `cor`, `dono`, `dinheiro`, `torcida`, `nomeEstadio`, `capacidade`, `vitoria`, `derrota`, `empate`, `pontos`) VALUES
-(1, 'dsa', 'da', 'yellow', 1, 10, '10', 'da', 10, 0, 0, 0, 0),
-(2, 'Time2', 'mascote2', 'cor2', NULL, 10, '10', 'time2', 10, 0, 0, 0, 0),
-(3, 'Time3', 'mascote3', 'cor3', NULL, 10, '10', 'time3', 10, 0, 0, 0, 0),
-(4, 'Time4', 'mascote4', 'cor4', NULL, 10, '10', 'time4', 10, 0, 0, 0, 0);
 
 --
 -- Índices de tabelas apagadas

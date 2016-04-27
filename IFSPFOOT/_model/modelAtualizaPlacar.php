2f00b010cc1f43b5a0d9dc3d0e849c554e80ce22
Wesley Almeida da Silva <wsyalmeida@gmail.com>
2016-04-27 19:22:48 -0300

    Salvando placares

--- IFSPFOOT/_model/modelAtualizaPlacar.php
+++ IFSPFOOT/_model/modelAtualizaPlacar.php
@@ -9,21 +9,21 @@
 	
 	$placar = explode(",", $placar);
 	
-	echo "TimeCasaJogo1 :".$placar[0];
-	echo "TimeCasaPlacarJogo1 :".$placar[1];
+	echo "TimeCasaJogo1 :".$placar[0]."<br>";
+	echo "TimeCasaPlacarJogo1 :".$placar[1]."<br>";
 	$golCasa = $placar[1];
 	
-	echo "TimeVisitantePlacarJogo1 :".$placar[3];
+	echo "TimeVisitantePlacarJogo1 :".$placar[3]."<br>";
 	$golVisitante =$placar[3];
-	echo "TimeVisitanteJogo1 :".$placar[4];
+	echo "TimeVisitanteJogo1 :".$placar[4]."<br>";
 	
-	echo "TimeCasaJogo2 :".$placar[6];
-	echo "TimeCasaPlacarJogo2 :".$placar[7];
+	echo "TimeCasaJogo2 :".$placar[6]."<br>";
+	echo "TimeCasaPlacarJogo2 :".$placar[7]."<br>";
 	$golCasa1 = $placar[7];
 	
-	echo "TimeVisitantePlacarJogo2 :".$placar[9];
+	echo "TimeVisitantePlacarJogo2 :".$placar[9]."<br>";
 	$golVisitante1 = $placar[9];
-	echo "TimeVisitanteJogo2 :".$placar[10];
+	echo "TimeVisitanteJogo2 :".$placar[10]."<br>";
 	
 
 	switch($rodadaAtual){
@@ -59,7 +59,7 @@
 		$preparaAtualizaPlacarJogo->bindValue(4,$rodadaAtual);
 		$preparaAtualizaPlacarJogo->execute();
 		
-		$consultaJogo = 'SELECT timeCasa, timeVisitante FROM Jogo WHERE id = ? ';
+		$consultaJogo = 'SELECT timeCasa, timeVisitante FROM Jogo WHERE id = ? ';	
 		$preparaConsultaJogo = $conn->prepare($consultaJogo);
 		$preparaConsultaJogo->bindValue(1, $i);
 		$preparaConsultaJogo->execute();
@@ -72,7 +72,7 @@
 			
 		}
 
-		$consultaTimeCasa = 'SELECT vitoria,empate,derrota,pontos FROM Time WHERE nome = ?';
+		$consultaTimeCasa = 'SELECT vitoria,derrota,empate,pontos FROM Time WHERE nome = ?';
 		$preparaConsultaTimeCasa = $conn->prepare($consultaTimeCasa);
 		$preparaConsultaTimeCasa->bindValue(1, $timeCasa);
 		$preparaConsultaTimeCasa->execute();
@@ -81,13 +81,13 @@
 		while ($row = $preparaConsultaTimeCasa->fetch()) {
 				
 			$vitoriaTimeCasa = $row[0];
-			$empateTimeCasa = $row[1];
-			$derrotaTimeCasa = $row[2];
+			$derrotaTimeCasa = $row[1];
+			$empateTimeCasa = $row[2];
 			$pontosTimeCasa = $row[3];
 				
 		}
 		
-		$consultaTimeVisitante = 'SELECT vitoria,empate,derrota,pontos FROM Time WHERE nome = ?';
+		$consultaTimeVisitante = 'SELECT vitoria,derrota,empate,pontos FROM Time WHERE nome = ?';
 		$preparaConsultaTimeVisitante = $conn->prepare($consultaTimeVisitante);
 		$preparaConsultaTimeVisitante->bindValue(1, $timeVisitante);
 		$preparaConsultaTimeVisitante->execute();
@@ -96,42 +96,79 @@
 		while ($row = $preparaConsultaTimeVisitante->fetch()) {
 		
 			$vitoriaTimeVisitante = $row[0];
-			$empateTimeVisitante = $row[1];
-			$derrotaTimeVisitante = $row[2];
+			$derrotaTimeVisitante = $row[1];
+			$empateTimeVisitante = $row[2];
 			$pontosTimeVisitante = $row[3];
 		
 		}
 		
+		/*
+		echo "vitorias time casa".$vitoriaTimeCasa."--------";
+		echo "derrotas time casa".$derrotaTimeCasa."---------------";
+		echo "empate time casa".$empateTimeCasa."---------------";
+		echo "Pontos time casa".$pontosTimeCasa."---------------";
+		
+		echo "vitorias time Visitante".$vitoriaTimeVisitante."--------";
+		echo "derrotas time Visitante".$empateTimeVisitante."--------";
+		echo "empate time Visitante".$derrotaTimeVisitante."--------";
+		echo "Pontos time Visitante".$pontosTimeVisitante."--------";
+		*/
 		if($golCasa > $golVisitante){
 			
 			$vitoriaTimeCasa++;
 			$pontosTimeCasa = $pontosTimeCasa+3;
+			$derrotaTimeCasa++;
 			
-			$atualizaTime = 'UPDATE Time SET  vitoria = ?, pontos = ? WHERE id = ? and nome = ?';
-			$preparaAtualizaTime = $conn->prepare($atualizaTime);
-			$preparaAtualizaTime->bindValue(1,$vitoriaTimeCasa);
-			$preparaAtualizaTime->bindValue(2,$pontosTimeCasa);
-			$preparaAtualizaTime->bindValue(3,$i);
-			$preparaAtualizaTime->bindValue(4,$timeCasa);
-			$preparaAtualizaTime->execute();
+			$atualizaTimeCasa1 = 'UPDATE Time SET  vitoria = ?, pontos = ? WHERE nome = ?';
+			$preparaAtualizaTimeCasaTabela1 = $conn->prepare($atualizaTimeCasa1);
+			$preparaAtualizaTimeCasaTabela1->bindValue(1,$vitoriaTimeCasa);
+			$preparaAtualizaTimeCasaTabela1->bindValue(2,$pontosTimeCasa);
+			$preparaAtualizaTimeCasaTabela1->bindValue(3,$timeCasa);
+			$preparaAtualizaTimeCasaTabela1->execute();
+			
+			$atualizaTime1 = 'UPDATE Time SET  derrota = ? WHERE nome = ?';
+			$preparaAtualizaTimeVisitanteTabela1 = $conn->prepare($atualizaTime1);
+			$preparaAtualizaTimeVisitanteTabela1 ->bindValue(1,$derrotaTimeCasa);
+			$preparaAtualizaTimeVisitanteTabela1 ->bindValue(2,$timeVisitante);
+			$preparaAtualizaTimeVisitanteTabela1 ->execute();
 			
 		}
-		elseif ($golVisitante < $golCasa){
+		else if ($golVisitante > $golCasa){
+			
 			$vitoriaTimeVisitante++;
 			$pontosTimeVisitante= $pontosTimeVisitante+3;
+			$derrotaTimeCasa++;
 				
-			$atualizaTime = 'UPDATE Time SET  vitoria = ?, pontos = ? WHERE id = ? and nome = ?';
-			$preparaAtualizaTime = $conn->prepare($atualizaTime);
-			$preparaAtualizaTime->bindValue(1,$vitoriaTimeVisitante);
-			$preparaAtualizaTime->bindValue(2,$pontosTimeVisitante);
-			$preparaAtualizaTime->bindValue(3,$i);
-			$preparaAtualizaTime->bindValue(4,$timeVisitante);
-			$preparaAtualizaTime->execute();
+			$atualizaTimeVisitante2 = 'UPDATE Time SET  vitoria = ?, pontos = ? WHERE nome = ?';
+			$preparaAtualizaTimeVisitanteTabela2 = $conn->prepare($atualizaTimeVisitante2);
+			$preparaAtualizaTimeVisitanteTabela2->bindValue(1,$vitoriaTimeVisitante);
+			$preparaAtualizaTimeVisitanteTabela2->bindValue(2,$pontosTimeVisitante);
+			$preparaAtualizaTimeVisitanteTabela2->bindValue(3,$timeVisitante);
+			$preparaAtualizaTimeVisitanteTabela2->execute();
+			
+			$atualizaTimeCasa2 = 'UPDATE Time SET  derrota = ? WHERE nome = ?';
+			$preparaAtualizaTimeCasaTabela2 = $conn->prepare($atualizaTimeCasa2);
+			$preparaAtualizaTimeCasaTabela2->bindValue(1,$derrotaTimeCasa);
+			$preparaAtualizaTimeCasaTabela2->bindValue(2,$timeCasa);
+			$preparaAtualizaTimeCasaTabela2->execute();
 				
 			
 		}
-		else {
+		else if($golCasa == $golVisitante) {
+			$empateTimeCasa++;
+			$empateTimeVisitante++;
 			
+			$atualizaTimeCasa3 = 'UPDATE Time SET  empate = ? WHERE nome = ?';
+			$preparaAtualizaTimeCasaTabela3 = $conn->prepare($atualizaTimeCasa3);
+			$preparaAtualizaTimeCasaTabela3->bindValue(1,$empateTimeCasa);
+			$preparaAtualizaTimeCasaTabela3->bindValue(2,$timeCasa);
+			$preparaAtualizaTimeCasaTabela3->execute();
+			
+			$atualizaTimeVisitante3 = 'UPDATE Time SET  empate = ? WHERE nome = ?';
+			$preparaAtualizaTimeVisitanteTabela3 = $conn->prepare($atualizaTimeVisitante3);
+			$preparaAtualizaTimeVisitanteTabela3 ->bindValue(1,$empateTimeVisitante);
+			$preparaAtualizaTimeVisitanteTabela3 ->bindValue(2,$timeVisitante);
+			$preparaAtualizaTimeVisitanteTabela3 ->execute();
 		}
 		
 			

ae3a6d2b3861d25d84f2b5473274c4f7eca2f4f3
Wesley Almeida da Silva <wsyalmeida@gmail.com>
2016-05-14 10:19:53 -0300

    Alterado menu

--- IFSPFOOT/_view/viewInicial.php
+++ IFSPFOOT/_view/viewInicial.php
@@ -1,10 +1,6 @@
 <?php
-
-	//Incluindo cabecalho na página
-	include_once "viewCabecalho.php";
-	echo "<hr></hr>";
+session_start();
 ?>
-
 <!DOCTYPE html>
 
 <html lang="PT-BR">
@@ -12,42 +8,77 @@
 <head>
 
 	<meta charset= "UTF-8"/>
-	<title>Página Inicial</title>
-	<link rel="stylesheet" href="_css/cssViewMenu.css">
-	<link rel="stylesheet" href="_css/cssView.css">
+	
+	<title>Menu</title>
+	
+	<!-- Visualização Mobile" -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	
 	<!-- Incluindo Bootstrap CSS -->
-	<link href="_view/_bootstrap-3.3.6-dist/_css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="_bootstrap-3.3.6-dist/_css/bootstrap.min.css" rel="stylesheet" media="screen">
 	
 	<!-- Incluindo Bootstrap JavaScript-->
-	<script src="_view/_bootstrap-3.3.6-dist/_js/bootstrap.min.js"></script>
+	<script src="_bootstrap-3.3.6-dist/_js/bootstrap.min.js"></script>
 	
 	<!-- Incluindo jquery-->
-	<script src="_view/_jquery/jquery.js"></script>
+	<script src="_jquery/jquery.js"></script>
+	
+	<!-- Incluindo jquery backstretch-->
+	<script src="_jquery/jquery.backstretch.min.js"></script>
+	
+	<!-- Incluindo js index-->
+	<script src="_js/jsIndex.js"></script>
+	
+	<!-- Incluindo css dos elementos-->
+   	<link rel="stylesheet" href="_css/form-elements.css">
+   	
+   	<!-- Incluindo css index-->
+    <link rel="stylesheet" href="_css/style.css">
+
 </head>
 
 <body>
-	
-	<header></header>
-	
-	<section>
-	
-		<nav>
-			<ul>
-				
-				<li><a href = "../_controller/controllerGerenciaInicio.php">Novo Jogo</a></li>
-				
-				<li><a href = "viewTelaTime.php">Carregar Jogo</a></li>
-				
-				<li><a href = "viewEditarJogo.php">Editar Jogo</a></li>
-				
-			</ul>
-
-		</nav>
-		 
-	 </section>
-	 <hr></hr>
-	 <footer><?php include_once "viewRodape.php"?></footer>
+	<!-- Top content -->
+	<div class="top-content">
+		
+	    <div class="inner-bg"style="margin-bottom: 0px; padding-bottom:0px;">
+	        <div class="container">
+	            
+	            <div class="row" >
+	                <div class="col-sm-6 col-sm-offset-3 form-box">
+	                	<div class="form-top" style="margin-top:-100px;">
+	                		<div class="form-top-left">
+	                			<h1 class="text-center" >MENU</h1>
+	                    		<h3 class="text-center">Selecione sua opção</h3>
+	                		</div>
+	                		<div class="form-top-right" >
+	                			<i class="fa fa-key"></i>
+	                		</div>
+	                    </div>
+	                    <div class="form-bottom" >
+		                	
+		                    <div class="social-login-buttons" style="margin-bottom: 0px; padding-bottom:0px;">
+		                		
+		                    	<div class="form-group" >
+			                		<a class="btn btn-info btn-block" href="../_controller/controllerGerenciaInicio.php">
+			                    		 Novo Jogo
+			                    	</a>
+		                    	</div>
+		                    	
+		                    	<div class="form-group">
+			                    	<a class="btn btn-warning btn-block" href="viewTelaTime.php">
+			                    		 Carregar Jogo
+			                    	</a>
+			                    </div>
+			                    
+	                    	</div>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	    
+	</div>
 	
 </body>
 

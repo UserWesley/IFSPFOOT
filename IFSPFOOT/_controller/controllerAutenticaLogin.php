249e8c94e4d8f4f33b6185c2311dda17278566da
Wesley Almeida da Silva <wsyalmeida@gmail.com>
2016-05-22 11:52:01 -0300

    controller orientado a objetos

--- IFSPFOOT/_controller/controllerAutenticaLogin.php
+++ IFSPFOOT/_controller/controllerAutenticaLogin.php
@@ -5,50 +5,77 @@
 	 se não, será informado no index "Usuário ou senha inexistentes no banco
 	 */
 
-	//Inclusão do arquivo para conexão com o banco de dados PDO
-	include_once '../_model/_bancodedados/modelBancodeDados.php';
-	
-	//Iniciando sessãoo
+	//Iniciando sessão
 	session_start();
-	
-	//Variaveis do index.php
-	$usuario = $_POST['form-username'];
-	$senha = $_POST['form-password'];
-	
-	//Consulta a Tabela Login, para autenticar usuário
-	$consultaLogin = 'SELECT id,usuario,senha FROM Login WHERE usuario = ? AND senha = ?';
-	$preparaConsultaLogin = $conn->prepare($consultaLogin);
-	$preparaConsultaLogin->bindValue(1, $usuario);
-	$preparaConsultaLogin->bindValue(2, $senha);
-	$preparaConsultaLogin->execute();
-	$dados = $preparaConsultaLogin->fetch(PDO::FETCH_OBJ);
-	
 
-	//Se o resultado da consulta for diferente de vazio, então registra sessão e chama a view Inicial
-	if(!empty($dados)){
+	//Inclusão do arquivo para model Classe do usuario
+	include_once '../_model/modelClassUsuario.php';
+	
+	class controllerAutenticaLogin {
 		
-		//Registra na sessão id do usuario
-		$consultaId = 'SELECT id FROM Login WHERE usuario = ?';
-		$preparaConsultaId = $conn->prepare($consultaId);
-		$preparaConsultaId->bindValue(1, $usuario);
-		$preparaConsultaId->execute();
-		$idDono = $preparaConsultaId->fetchColumn(0);
-	
-		$_SESSION['usuario']= $usuario;
-		$_SESSION['senha']= $senha;
-		$_SESSION['logado']= true;
-		$_SESSION['idDono']= $idDono;
+		private $login;
+		private $senha;
 		
-		header("Location: ../_view/viewInicial.php");
-	 
-	}
-	else {
-		$_SESSION['logado']= false;
-		header("Location: ../index.php");
+		function __construct(){
+			$this->consultaAcesso();
+		}
+		
+		function __destruct(){
+			
+		}
+		
+		public function getLogin(){
+			return $this->login;
+		}
+		
+		public function setLogin($login){
+			$this->login = $login;
+		}
+		
+		public function getSenha(){
+			return $this->senha;
+		}
+		
+		public function setSenha($senha){
+			$this->senha = $senha;
+		}
+		
+		function consultaAcesso(){
+			
+			//Variaveis do index.php
+			$this->setLogin($_POST['form-username']);
+			$this->setSenha($_POST['form-password']);
+			
+			$usuario = new modelClassUsuario();
+			
+			$usuario->setLogin($this->getLogin());
+			$usuario->setSenha($this->getSenha());
+			
+			$dados = $usuario->consultaUsuario($usuario);
+			
+		 	//Se o resultado da consulta for diferente de vazio, então registra sessão e chama a view Inicial
+			if(!empty($dados)){
+
+				//$idDono = $usuario->autenticaUsuario();
+				
+				$_SESSION['usuario']= $usuario;
+				$_SESSION['logado']= true;
+				$_SESSION['idDono']= $idDono;
+				
+				header("Location: ../_view/viewInicial.php");
+			 
+			}
+			
+			else {
+				
+				$_SESSION['logado']= false;
+				header("Location: ../index.php");
+		
+			}
+		}
 
 	}
 	
-	//Fecha conexão
-	$conn = null;
-
+	$autenticaLogin = new controllerAutenticaLogin();
+	
 ?>

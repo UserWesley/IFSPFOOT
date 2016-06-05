739a9ada872ba002430f4c89ce3e1e67441dd966
Wesley Almeida da Silva <wsyalmeida@gmail.com>
2016-06-04 21:53:23 -0300

    Controller para cadastrar novo usuario

---
+++ IFSPFOOT/_controller/controllerClassCadastraUsuario.php
@@ -0,0 +1,54 @@
+<?php 
+
+	
+	include_once '../_model/modelClassUsuario.php';
+	session_start();
+	
+	class controllerClassCadastraUsuario{
+		
+		public function __construct(){
+			
+			$this->verificaDados();
+			$this->cadastrarUsuario();
+		}
+		
+		public function verificaDados(){
+			
+			$usuario = new modelClassUsuario();
+			
+			$verificaUsuario = $usuario->verificaLogin($_POST['form-login']);
+			$verificaSenha = $usuario->verificaSenha($_POST['form-senha'], $_POST['form-repitasenha']);
+			
+			if($verificaUsuario != NULL){
+				
+				$_SESSION['cadastroLogin'] = "Login Sendo Utilizado !";
+				header("Location: ../_view/viewCadastroNovoUsuario.php");
+				
+			}
+			
+			if($verificaSenha == false){
+				
+				$_SESSION['cadastroSenha'] = "Senha Invalida";
+				header("Location: ../_view/viewCadastroNovoUsuario.php");
+			}
+		}
+		public function cadastrarUsuario(){
+			
+			$usuario = new modelClassUsuario();
+				
+			$usuario->setLogin($_POST['form-login']);
+			$usuario->setSenha($_POST['form-senha']);
+			$usuario->setNome($_POST['form-nome']);
+			$usuario->setSobrenome($_POST['form-sobrenome']);
+			$usuario->setEmail($_POST['form-email']);
+			$usuario->setCelular($_POST['form-celular']);
+			$usuario->cadastraUsuario($usuario);
+			
+				
+		}
+	}
+	
+	$user = new controllerClassCadastraUsuario();
+
+	
+?>

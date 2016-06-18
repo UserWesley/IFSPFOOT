c719584b47293ea01184601fb9e919d581ce3bb5
Wesley Almeida da Silva <wsyalmeida@gmail.com>
2016-06-18 15:21:34 -0300

    Add temperamento sorteado

--- IFSPFOOT/_model/modelClassTemperamento.php
+++ IFSPFOOT/_model/modelClassTemperamento.php
@@ -1,4 +1,7 @@
 <?php 
+	
+	//Inclusão do arquivo para conexão com o banco de dados PDO
+	include_once '../_model/_bancodedados/modelBancodeDadosConexao.php';
 
 	Class modelClassTemperamento{
 		
@@ -20,6 +23,36 @@
 		
 		public function setNome($nome){
 			$this->nome = $nome;
+		}
+		
+		//Consulta Temperamento
+		public function consultaTemperamento(){
+				
+			$temperamentosDisponiveis = array();
+		
+			$conn = Database::conexao();
+		
+			$consultaTemperamento = 'SELECT id FROM Temperamento';
+			$preparaConsultaTemperamento= $conn->query($consultaTemperamento);
+			$preparaConsultaTemperamento->execute();
+		
+			$result = $preparaConsultaTemperamento->setFetchMode(PDO::FETCH_NUM);
+			while ($row = $preparaConsultaTemperamento->fetch()) {
+					
+				$temperamentosDisponiveis[]= $row[0];
+					
+			}
+				
+			return $temperamentosDisponiveis;
+				
+		}
+		//Sortea um temperamento
+		public function sortearTemperamento($temperamentosDisponiveis){
+				
+			$valorMaximo = count($temperamentosDisponiveis);
+			$nome = rand(1,$valorMaximo);
+				
+			return $nome;
 		}
 		
 	}

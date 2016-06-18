11fd1085bd484fb14f13fc00766b739cc5d39caa
Wesley Almeida da Silva <wsyalmeida@gmail.com>
2016-06-18 12:08:49 -0300

    prototipo criado

--- IFSPFOOT/_view/viewEstrategia.php
+++ IFSPFOOT/_view/viewEstrategia.php
@@ -3,11 +3,7 @@
 	/*Arquivo será responsável por salvo no banco as informações de estratégia*/
 	
 	session_start();
-	
-	//Inclusão do arquivo para conexão com o banco de dados PDO
-	include_once '../_model/_bancodedados/modelBancodeDados.php';
-	
-	$idTime = $_SESSION['idDono'];
+	include_once '../_controller/controllerClassMenu.php';
 	
 ?>
 
@@ -37,90 +33,56 @@
 <body>
 
 	<form>
-	
-		<h1 class="text-center">Estratégia</h1>
-		
+		<h1 class="text-center">Definição Tática do Time</h1>
 		<h3 class="text-center">Formação</h3>
-		<select id="idFormacao"> 
-			<option value="1">5-4-1</option>
-			<option value="2">4-5-1</option>
-			<option value="3">4-4-2</option>
-			<option value="4">4-4-3</option>
-			<option value="5">3-5-2</option>
-		</select>
-		
-		<h3 class="text-center">Estilo de jogo</h3>
-		<select id="idEstiloJogo"> 
-			<option value="1">Ataque Total</option>
-			<option value="2">Jogo Equilibrado</option>
-			<option value="3">Contra-ataque</option>
-			<option value="4">Defesa Total</option>
-		</select>
-	
-	</form>
-	<!-- 
-	<h3 class="text-center">Selecionar Titulares</h3>
-	<div class="table-responsive">
-    	<table class="table">
-      		<thead>
-        		<tr class = "info">
-        			  <th>Titular</th>
-			          <th>Nome</th>
-			          <th>Sobrenome</th>
-			          <th>Posição</th>
-			          <th>Nacionalidade</th>
-			          <th>Habilidade</th>
-			          <th>Idade</th>
-			          <th>Forca</th>
-			          <th>Estamina</th>
-		          	  <th>Nivel</th>
-		          	  <th>Gol</th>
-        		</tr> 
-      		</thead>
-      		<tbody>
-      <?php
-      	
-      	$consultaTime = 'SELECT id FROM Time WHERE dono = ? ';
-      	$preparaConsultaTime = $conn->prepare($consultaTime);
-      	$preparaConsultaTime->bindValue(1,$idTime);
-      	$preparaConsultaTime->execute();
-      
-      	$result = $preparaConsultaTime->setFetchMode(PDO::FETCH_NUM);
-      
-      	while ($row = $preparaConsultaTime->fetch()) { 
-      		$idTimeJogador = $row[0];     
-      		
-      	}
-      	//Listando Jogadores do time
-        $consultaJogador = 'SELECT titular,nome ,sobrenome,posicao,nacionalidade,habilidade ,idade ,forca ,estamina ,nivel,gol FROM Jogador WHERE idTime = ? ';
-		$preparaConsultaJogador = $conn->prepare($consultaJogador);
-		$preparaConsultaJogador->bindValue(1, $idTimeJogador);
-		$preparaConsultaJogador->execute();
-		
-		$result = $preparaConsultaJogador->setFetchMode(PDO::FETCH_NUM);
-		
-		while ($row = $preparaConsultaJogador->fetch()) {
+		<?php
+		 
+			$controllerMenu = new controllerClassMenu();
+			
+			$estrategia = array();
+			$formacao = array();
+			$agressividade = array();
+			
+			$estrategia = $controllerMenu->buscaEstrategia();
+			$formacao = $controllerMenu->buscaFormacao();
+			$agressividade= $controllerMenu->buscaAgressividade();		
 
-            echo '<tr  class = "active">';
-            echo "<td>{$row[0]}</td>";            
-            echo "<td>{$row[1]}</td>";
-            echo "<td>{$row[2]}</td>";  
-            echo "<td>{$row[3]}</td>";
-            echo "<td>{$row[4]}</td>";
-            echo "<td>{$row[5]}</td>";
-            echo "<td>{$row[6]}</td>";
-            echo "<td>{$row[7]}</td>";
-            echo "<td>{$row[8]}</td>";
-            echo "<td>{$row[9]}</td>";
-            echo "<td>{$row[10]}</td>";
-            echo '</tr>';
-          }
-      ?>
-      
-        </tbody>
-      </table>
-	</div>
-	 -->
+			echo "<select id = \"idSelectFormacao\" name = \"selectFormacao\">";
+				
+			for($i=0; $i < 5; $i++) {
+			
+				echo "<option value = ".current($formacao).">".next($formacao)."</option>";
+				next($formacao);
+			}
+				
+			echo "</select>";
+			
+		?>
+			<h3 class="text-center">Estratégia</h3>
+		<?php
+			echo "<select id = \"idSelectEstrategia\" name = \"selectEstrategia\">";
+			
+			for($i=0; $i < 4; $i++) {
+					
+				echo "<option value = ".current($estrategia).">".next($estrategia)."</option>";
+				next($estrategia);
+			}
+			
+			echo "</select>";
+		?>
+			<h3 class="text-center">Agressividade</h3>
+		<?php	
+			echo "<select id = \"idSelectAgressividade\" name = \"selectAgressividade\">";
+				
+			for($i=0; $i < 3; $i++) {
+					
+				echo "<option value = ".current($agressividade).">".next($agressividade)."</option>";
+				next($agressividade);
+			}
+				
+			echo "</select>";
+		?>
+
 </body>
 
 </html>

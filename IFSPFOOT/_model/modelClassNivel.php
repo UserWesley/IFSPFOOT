13d4a44c9a6dba1ffc9c919476b2b28668fb9d0b
Wesley Almeida da Silva <wsyalmeida@gmail.com>
2016-06-18 15:22:06 -0300

    add nivel sortea nivel

---
+++ IFSPFOOT/_model/modelClassNivel.php
@@ -0,0 +1,58 @@
+<?php
+
+	//Inclusão do arquivo para conexão com o banco de dados PDO
+	include_once '../_model/_bancodedados/modelBancodeDadosConexao.php';
+	
+	Class modelClassNivel{
+	
+		private $id;
+		private $nome;
+	
+		//Getters e setters
+		public function getId(){
+			return $this->id;
+		}
+	
+		public function setId($id){
+			$this->id = $id;
+		}
+	
+		public function getNome(){
+			return $this->nome;
+		}
+	
+		public function setNome($nome){
+			$this->nome = $nome;
+		}
+	
+		//Consulta Nivel
+		public function consultaNivel(){
+	
+			$niveisDisponiveis = array();
+	
+			$conn = Database::conexao();
+	
+			$consultaNivel = 'SELECT id FROM Nivel';
+			$preparaConsultaNivel= $conn->query($consultaNivel);
+			$preparaConsultaNivel->execute();
+	
+			$result = $preparaConsultaNivel->setFetchMode(PDO::FETCH_NUM);
+			while ($row = $preparaConsultaNivel->fetch()) {
+					
+				$niveisDisponiveis[]= $row[0];
+					
+			}
+	
+			return $niveisDisponiveis;
+	
+		}
+		//Sortea um posicao
+		public function sortearNivel($niveisDisponiveis){
+	
+			$valorMaximo = count($niveisDisponiveis);
+			$nome = rand(1,$valorMaximo);
+	
+			return $nome;
+		}
+	
+	}

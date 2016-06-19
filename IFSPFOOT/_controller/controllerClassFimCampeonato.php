d467f9e3b76585cda6a81ffdb8716797a03b9dda
Wesley Almeida da Silva <wsyalmeida@gmail.com>
2016-06-19 09:13:46 -0300

    Pequenas correçoes

---
+++ IFSPFOOT/_controller/controllerClassFimCampeonato.php
@@ -0,0 +1,20 @@
+<?php
+	
+	//Inclusão da classe ModelUsuário
+	include_once '../_model/modelClassTime.php';
+	include_once '../_model/modelClassJogador.php';
+
+	session_start();
+
+	Class controllerClassFimCampeonato{
+ 	
+	 	public function BuscarTimeVencedor(){
+	 		
+	 	}
+	 	
+	 	public function BuscarArtilheiro(){
+	 		
+	 	}
+ 	
+ }
+?>
